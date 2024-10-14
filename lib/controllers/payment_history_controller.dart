190cd26cda935d7fcf7afe7179880fb50b510ea0
Mafaka77 <zuala4@gmail.com>
2024-10-14 16:25:19 +0530

    asdasd

--- lib/controllers/payment_history_controller.dart
+++ lib/controllers/payment_history_controller.dart
@@ -4,16 +4,24 @@ import 'package:rti_new_apps/services/payment_history_services.dart';
 class PaymentHistoryController extends GetxController {
   var isLoading = false.obs;
   PaymentHistoryServices services = Get.find(tag: 'paymentHistoryServices');
+  var mobilePayments = [].obs;
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
   }
 
-  void getAllPaymentHistory() async {
+  void getMobilePayment() async {
     isLoading.value = true;
+    mobilePayments.clear();
     try {
-      var response = await services.getAllPayment();
+      var response = await services.getMobilePayment();
+      if (response.statusCode == 200) {
+        if (response.data['status'] == 200) {
+          var data = response.data['mobilePayments'];
+          mobilePayments.addAll(data);
+        }
+      }
     } catch (ex) {}
   }
 }
--- lib/services/payment_history_services.dart
+++ lib/services/payment_history_services.dart
@@ -2,9 +2,12 @@ import 'package:rti_new_apps/services/base_services.dart';
 import 'package:rti_new_apps/services/routes.dart';
 
 class PaymentHistoryServices extends BaseService {
-  Future getAllPayment() async {
+  Future getMobilePayment() async {
     try {
       var response = await client.get(Routes.GET_ALL_PAYMENT_HISTORY);
-    } catch (ex) {}
+      return response;
+    } catch (ex) {
+      return Future.error(ex);
+    }
   }
 }
