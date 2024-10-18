ab24d632edd60b0e43ed66abf52dbfc82e5afc69
Mafaka77 <zuala4@gmail.com>
2024-10-18 15:28:50 +0530

    asdasd

--- lib/controllers/track_controller.dart
+++ lib/controllers/track_controller.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class TrackController extends GetxController {
-  RefreshController refreshController =
-      RefreshController(initialRefresh: false);
   var isRtiSelected = true.obs;
   var isComplainSelected = false.obs;
 }
